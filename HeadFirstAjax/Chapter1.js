467f732eb60bd109888805c64265bbf2aca5f6a5
akaxiaok <akaxiaok@gmail.com>
2017-12-20 22:52:01 +0800

    feature: HeadFirstAjax Chapter1

---
+++ HeadFirstAjax/Chapter1.js
@@ -0,0 +1,50 @@
+(function () {
+  var baseURL = 'https://api.github.com';
+  var GET = "GET";
+  var POST = "POST";
+
+  function createRequest() {
+    var request = {};
+    if (XMLHttpRequest) {
+      return new XMLHttpRequest();
+    }
+    if (ActiveXObject) {
+      try {
+        request = new ActiveXObject('Msxml2.XMLHTTP');
+      }
+      catch (otherMS) {
+        try {
+          request = new ActiveXObject('Microsoft.XMLHTTP');
+        } catch (error) {
+          return null;
+        }
+      }
+    }
+    return null;
+  }
+
+  function getDetail(name) {
+    var request = createRequest();
+
+    function displayDetails() {
+      if (request.readyState === 4) {
+        if (request.status === 200) {
+          var detailDiv = document.createElement('div');
+          detailDiv.innerHTML = request.responseText;
+          document.body.appendChild(detailDiv);
+        }
+      }
+    }
+
+    if (!request) {
+      alert('can not use ajax');
+      return;
+    }
+    var url = baseURL + '/users/' + encodeURI(name);
+    request.open(GET, url, true);
+    request.onreadystatechange = displayDetails;
+    request.send();
+  }
+  getDetail('akaxiaok');
+})();
+
