73502a567da83a88f603336cc7341df971cc0230
Paradox <akaxiaok@gmail.com>
2019-05-21 22:28:52 +0800

    Test lazy load

--- type-script/src/App.tsx
+++ type-script/src/App.tsx
@@ -10,31 +10,41 @@ const Memo = React.memo(function MyMemo(props: { value: number }) {
   const value = props.value;
   return (<div>
     {value}
-  </div>)
+  </div>);
 });
+
+
+// import() is ECMAScript proposal. it returns a Promise
+const Big = React.lazy(() => import('./components/Big'));
+const Suspense = React.Suspense;
+
 
 class App extends React.Component {
   public static value: number = 1;
   public state = {
-    value:1
+    value: 1
   };
+
   public componentDidMount(): void {
     setInterval(() => {
-      this.setState({value: 1})
-    }, 1000)
+      this.setState({value: 1});
+    }, 1000);
   }
 
   public render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo"/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.tsx</code> and save to reload.
         </p>
         <Memo value={this.state.value}/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Big/>
+        </Suspense>
       </div>
     );
   }
---
+++ type-script/src/components/Big.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+
+
+class Big extends React.Component {
+  public render(): React.ReactNode {
+    return (
+      <div>
+        some big component
+      </div>
+    )
+  }
+}
+
+export default Big;
