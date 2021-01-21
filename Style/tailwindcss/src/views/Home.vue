da89aab2802207b068b2859ac2a488bc79d64fdb
akaxiok@gmail.com <kimi2923601>
2021-01-21 22:34:12 +0800

    feature: init tailwindcss project

---
+++ Style/tailwindcss/src/views/Home.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="home">
+    x
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {}
+}
+</script>
+<style scoped lang="scss">
+  @tailwind base;
+
+  @tailwind components;
+
+  @tailwind utilities;
+  @tailwind screens;
+
+  .home {
+    transform: scale();
+    @apply flex bg-teal-300 text-3xl;
+  }
+
+  @screen xl {
+    .home {
+      @apply bg-red-300 ;
+    }
+  }
+</style>
